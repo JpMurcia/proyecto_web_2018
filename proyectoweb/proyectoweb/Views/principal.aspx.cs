a7fe29c5ce1774f304a854040c12ded465693808
JpMurcia <juan.murcia@udla.edu.co>
2018-10-17 15:26:43 -0500

    sadsa
    asdsadasd

--- proyectoweb/proyectoweb/Views/principal.aspx.cs
+++ proyectoweb/proyectoweb/Views/principal.aspx.cs
@@ -16,17 +16,17 @@ namespace proyectoweb.Views
 
         protected void verGiecom_Click(object sender, EventArgs e)
         {
-            Response.Redirect("FormularioDePagina2.aspx?grupo=giecom");
+            Response.Redirect("FormularioDePagina1.aspx?grupo=giecom");
         }
 
         protected void verSara_Click(object sender, EventArgs e)
         {
-            Response.Redirect("FormularioDePagina2.aspx?grupo=sara");
+            Response.Redirect("FormularioDePagina1.aspx?grupo=sara");
         }
 
         protected void verPicardie_Click(object sender, EventArgs e)
         {
-            Response.Redirect("FormularioDePagina2.aspx?grupo=picardie");
+            Response.Redirect("FormularioDePagina1.aspx?grupo=picardie");
         }
     }
 }
