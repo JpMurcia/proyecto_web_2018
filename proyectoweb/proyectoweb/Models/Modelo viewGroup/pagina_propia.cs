9abb25f762b9bc2eaa96565d2d67202be8cb0268
JpMurcia <juan.murcia@udla.edu.co>
2019-03-04 18:14:01 -0500

    Update pagina_propia.cs

--- proyectoweb/proyectoweb/Models/Modelo viewGroup/pagina_propia.cs
+++ proyectoweb/proyectoweb/Models/Modelo viewGroup/pagina_propia.cs
@@ -1,11 +1,37 @@
-﻿using System;
+﻿using proyectoweb.Models.conexion;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 
 namespace proyectoweb.Models.Modelo_viewGroup
 {
+
+  
+
     public class pagina_propia
     {
+
+        private ConexionPropio conect = new ConexionPropio();
+
+
+
+
+
+
+        public DataTable consul_dato()
+        {
+            List<Parametro> p = new List<Parametro>();
+
+
+            return conect.ExecuteProcedure("vista_apropia", p);
+
+
+        }
+
+
     }
+
+    
 }
