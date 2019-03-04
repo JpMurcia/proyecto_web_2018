1e04b16ea87b4f7e1094d04217fbbbfff3140896
JpMurcia <juan.murcia@udla.edu.co>
2019-03-04 18:13:46 -0500

    Update indexController.cs

--- proyectoweb/proyectoweb/Controllers/indexController.cs
+++ proyectoweb/proyectoweb/Controllers/indexController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using proyectoweb.Models.Modelo_viewGroup;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -8,8 +9,14 @@ namespace proyectoweb.Controllers
 {
     public class indexController
     {
+        pagina_propia pag = new pagina_propia();
 
-        
-       
+
+        public DataTable dato_pag() {
+
+            return pag.consul_dato();
+        }
+
+
     }
 }
